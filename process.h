fb2b9339ac3b8d575a1bb4b18e77887f2f60bb75
L Grissom <126531867+laurgris@users.noreply.github.com>
2023-03-16 00:50:02 -0500

    Update process.h

--- process.h
+++ process.h
@@ -3,7 +3,6 @@
 
 typedef enum priority { FOREGROUND, BACKGROUND } priority; 
 
-/* DO NOT directly access any data inside of a processData struct */
 typedef struct processData{ int heap[  30        ]; char       PN21[       21];char TLN        [4  ];}   processData ;
 
 /* struct process (only accessed in student written code)
@@ -11,17 +10,10 @@ typedef struct processData{ int heap[  30        ]; char       PN21[       21];c
  * The data associated with a specific process.
  * At minimum you need to track the process name,
  * and a pointer to the processData struct returned 
- * by initializeProcessData,
- *
- * Hint: It may help to store how many time steps happened 
- * before a process was added to the schedule.
- *
- * You may also want to track the number of steps remaining until 
- * your FOREGROUND process is moved to the back of the queue.
+ * by initializeProcessData
  */
 typedef struct process
 {
-    //TODO: Put the data for your process here!
 	char* name;
 	processData* data;
 	priority prior;
