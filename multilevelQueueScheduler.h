5c19277510f42e9106ad9c27462bdafb928e1606
L Grissom <126531867+laurgris@users.noreply.github.com>
2023-03-16 00:48:50 -0500

    Update multilevelQueueScheduler.h

--- multilevelQueueScheduler.h
+++ multilevelQueueScheduler.h
@@ -13,14 +13,11 @@
  *
  * Data related to the order the processes should be scheduled
  * foreQueue and backQueue are the FOREGROUND and BACKGROUND queues respectively.
- *
- * Hint: It may also help to track the number of time steps that have been processed so far.
  */
 typedef struct schedule
 {
     Queue *foreQueue;
     Queue *backQueue;
-    //TODO: Put the data for your schedule program here!
 	int stepsTotal;
 
 
